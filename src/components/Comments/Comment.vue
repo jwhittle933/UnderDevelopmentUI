4d6e422f06444999a9e217373d9f3e7ae2878487
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-05-29 09:18:20 -0400

    comment

---
+++ src/components/Comments/Comment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ msg }}{{ commentId }}</h1>
+  </div>
+</template>
+
+<script>
+export const Comment = {
+  name: 'Comment ',
+  data() {
+    return {
+      msg: 'This is a single Comment',
+    }
+  },
+  computed: {
+    commentId() {
+      return this.$route.params.id
+    },
+  },
+}
+
+export default Comment
+</script>
+
+<style scoped></style>
---
+++ src/components/Comments/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export const Comments = {
+  name: 'CommentsComponent',
+  data() {
+    return {
+      msg: 'This is the Comments Component.',
+    }
+  },
+}
+
+export default Comments
+</script>
+
+<style scoped></style>
