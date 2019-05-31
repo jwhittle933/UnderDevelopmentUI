f88b6677f575bb7ad8202c52c050d5ed91e8cb82
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-05-31 09:32:05 -0400

    landing page expansions

--- src/components/Landing/TopPosts.vue
+++ src/components/Landing/TopPosts.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="top-posts-container">
     <div class="title-block">
-      Top Posts
+      <span>Top Posts</span>
+      <p>
+        This is where we'll offer a brief description like "Read our top posts".
+      </p>
+      <p>
+        This block will also contain a search bar/select menu for picking tags.
+      </p>
     </div>
     <div class="content-block">
-      Posts
+      <span>Posts</span>
+      <p>
+        This where we'll render the posts that the api sends in tiles. Perhaps
+        3x6 or 4x6 grid area.
+      </p>
     </div>
   </div>
 </template>
@@ -30,6 +40,13 @@ export default TopPosts
 </script>
 
 <style scoped>
+span {
+  font-size: 2em;
+}
+p {
+  font-size: 1em;
+}
+
 .top-posts-container {
   display: grid;
   grid-template-row: 100%;
@@ -37,5 +54,15 @@ export default TopPosts
   height: 30em;
   background-color: rgb(0, 0, 0);
   margin: 0;
+  padding: 3em;
+}
+
+.title-block {
+  color: #f1f1f1;
+  text-align: left;
+}
+
+.content-block {
+  color: #f1f1f1;
 }
 </style>
