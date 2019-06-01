59f83451f4941dad63537914246b8db57db7b89c
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-05-31 21:47:40 -0400

    start fixtures

---
+++ src/Utils/fixtures/Users.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export const Users = {}
+
+Users.admin = {
+  id: 1,
+  name: 'admin',
+  admin: true,
+  email: 'admin@user.com',
+  bio: 'This is the admin account for root user access to all resources.',
+  password_hash: '123fn2#$slkjdf-28j#',
+}
+Users.user1 = {
+  id: 2,
+  name: 'user 1',
+  admin: false,
+  email: 'user1@user.com',
+  bio:
+    'Ambitious programmer with lots of experience making apps that work most of the time but are completely finished.',
+  password_hash: '@#$sdFeff-sefje23f_er',
+}
