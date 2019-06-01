5627171151dfeae689127997241804a725e9eed5
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-05-31 21:53:23 -0400

    user fixture

--- src/Utils/fixtures/Users.js
+++ src/Utils/fixtures/Users.js
@@ -15,6 +15,6 @@ Users.user1 = {
   admin: false,
   email: 'user1@user.com',
   bio:
-    'Ambitious programmer with lots of experience making apps that work most of the time but are completely finished.',
+    'Ambitious programmer with lots of experience making apps that work most of the time.',
   password_hash: '@#$sdFeff-sefje23f_er',
 }
