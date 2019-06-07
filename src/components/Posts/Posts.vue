026c7b4523bd82e9a0fba86f9495b1fc5f721a80
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-06-07 05:33:43 -0400

    all posts head

--- src/components/Posts/Posts.vue
+++ src/components/Posts/Posts.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="posts-view">
-    <div class="posts-head"></div>
+    <div
+      class="posts-head"
+      :style="{
+        'background-image': 'url(' + image + ')',
+      }"
+    >
+      <span class="head-text">All Posts</span>
+    </div>
     <div class="posts-wrapper">
-      <div class="post-tiles" v-for="post in posts" :key="post.id">
-        {{ post }}
-      </div>
+      <!-- <div class="post-tiles" v-for="post in posts" :key="post.id"> -->
+      <!--   {{ post }} -->
+      <!-- </div> -->
     </div>
   </div>
 </template>
@@ -17,6 +24,7 @@ export const Posts = {
   data() {
     return {
       posts: [],
+      image: require('@/assets/all_posts.jpg'),
     }
   },
   mounted() {
@@ -27,4 +35,18 @@ export const Posts = {
 export default Posts
 </script>
 
-<style scoped></style>
+<style scoped>
+.posts-head {
+  display: flex;
+  width: 100%;
+  height: 30em;
+  background-position: center center;
+  background-size: cover;
+}
+
+.head-text {
+  font-size: 5em;
+  align-self: center;
+  margin-left: 2em;
+}
+</style>
