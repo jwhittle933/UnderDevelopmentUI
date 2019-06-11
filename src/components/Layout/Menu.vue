f011bf70f78df984aca5ff2485bcd6eb2015b36d
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-06-11 10:15:47 -0400

    layout changes

--- src/components/Layout/Menu.vue
+++ src/components/Layout/Menu.vue
@@ -1,45 +1,28 @@
 <template>
   <div class="menu-wrapper-outer">
     <div class="menu-wrapper-inner">
-        <transition name="slide">
-        <div
-        class="menu-links"
-        @mouseover="infoHover = false"
-        @mouseleave="infoHover = false"
-        >
-        <div class="main-link" @click="goInfo">Info</div>
-        <ul v-if="infoHover" class="dropdown-menu">
-            <li
-            class="dropdown-link"
-            v-for="link in infoLinks"
-            :key="link"
-            >{{ link.text }}</li>
-        </ul>
+      <transition name="slide">
+        <div class="menu-links" @mouseover="infoHover = false" @mouseleave="infoHover = false">
+          <div class="main-link" @click="goInfo">Info</div>
+          <ul v-if="infoHover" class="dropdown-menu">
+            <li class="dropdown-link" v-for="link in infoLinks" :key="link">{{ link.text }}</li>
+          </ul>
         </div>
-        </transition>
-        <transition name="slide">
-        <div
-        class="menu-links"
-        @mouseover="postHover = false"
-        @mouseleave="postHover = false"
-        >
-        <div class="main-link" @click="goPosts">Posts</div>
-        <ul v-if="postHover" class="dropdown-menu">
-            <li
-            class="dropdown-link"
-            v-for="link in postLinks"
-            :key="link"
-            >{{ link.text }}</li
-            >
-        </ul>
+      </transition>
+      <transition name="slide">
+        <div class="menu-links" @mouseover="postHover = false" @mouseleave="postHover = false">
+          <div class="main-link" @click="goPosts">Posts</div>
+          <ul v-if="postHover" class="dropdown-menu">
+            <li class="dropdown-link" v-for="link in postLinks" :key="link">{{ link.text }}</li>
+          </ul>
         </div>
-        </transition>
-        <transition name="slide">
+      </transition>
+      <transition name="slide">
         <div class="menu-links">
-        <div class="main-link" @click="goContact">Contact Us</div>
+          <div class="main-link" @click="goContact">Contact Us</div>
         </div>
-        </transition>
-      </div>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -49,62 +32,53 @@ export const Menu = {
   data() {
     return {
       links: [
-          { name: `Info`,
-            path: '/info'
-          }, 
-          { name: `Posts`,
-            path: '/posts'
-          }, 
-          { name: `Contact Us`,
-            path: '/contact-us'
-          }
-        ],
+        { name: `Info`, path: '/info' },
+        { name: `Posts`, path: '/posts' },
+        { name: `Contact Us`, path: '/contact-us' },
+      ],
       infoLinks: [
         {
           text: 'About Us',
-          path: '/about-us'
+          path: '/about-us',
         },
         {
           text: 'Authors',
-          path: '/authors'
-        }
-        ],
+          path: '/authors',
+        },
+      ],
       postLinks: [
         {
-          text: 'Top Posts', 
-          path: '/top-posts'
+          text: 'Top Posts',
+          path: '/top-posts',
         },
         {
           text: 'Development',
-          path: '/development'
+          path: '/development',
         },
-        { text: 'Christian Living',
-          path: 'christian-living'
-        }
+        { text: 'Christian Living', path: 'christian-living' },
       ],
       infoHover: false,
       postHover: false,
-      unrollMenu: false, 
+      unrollMenu: false,
     }
   },
   methods: {
-        goInfo: function() {
-            this.$router.push('/info')
-        },
-        goPosts: function() {
-            this.$router.push('/posts')
-        },
-        goContact: function() {
-            this.$router.push('/contact-us')
-        }
-    }
+    goInfo: function() {
+      this.$router.push('/info')
+    },
+    goPosts: function() {
+      this.$router.push('/posts')
+    },
+    goContact: function() {
+      this.$router.push('/contact-us')
+    },
+  },
 }
 
 export default Menu
 </script>
 
 <style scoped>
-
 .menu-wrapper-outer {
   display: flex;
   justify-content: center;
@@ -116,11 +90,11 @@ export default Menu
 }
 
 .menu-wrapper-inner {
-    display: flex; 
-    flex-direction: column; 
-    justify-content: space-between;
-    background-color: rgba(0,0,0,0);
-    margin-left: 1em;  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0);
+  margin-left: 1em;
 }
 
 .menu-links {
@@ -134,18 +108,18 @@ export default Menu
 .main-link {
   height: 30px;
   width: 75px;
-  padding-bottom: 1.75em; 
+  padding-bottom: 1.75em;
   border: none;
   color: inherit;
   background-color: inherit;
-  font-size: 1.1em; 
+  font-size: 1.1em;
 }
 
 .dropdown-menu {
   padding: 0;
   margin: 0;
   position: absolute;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   color: inherit;
   text-decoration: none;
   background-color: inherit;
@@ -158,5 +132,4 @@ export default Menu
 ul {
   color: #eee;
 }
-
 </style>
