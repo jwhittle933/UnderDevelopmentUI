103dd5bc8ff1afbd016654d4b1f2bb7d62020739
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-06-18 15:22:09 -0400

    restructure store

--- src/store/store.js
+++ src/store/store.js
@@ -3,41 +3,49 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const state = {
+  user: {
+    isLoggedIn: false,
+    userCredientials: {
+      userName: '',
+      userId: '',
+    },
+  },
+  listStyle: 'tile',
+}
+
+const mutations = {
+  loginUser: ({ user }, { username, password }) => {
+    return { user, username, password }
+  },
+  logoutuser: ({ user }) => {
+    return user
+  },
+  setPostsDisplay(state, userSelection) {
+    state.listStyle = userSelection
+  },
+}
+
+const actions = {
+  loginUser: ({ commit }) =>
+    commit('loginUser', {
+      username: 'testUser',
+      password: 'testPass',
+    }),
+  logoutUser: ({ commit }) => commit('logoutUser'),
+  setPostsDisplay: ({ commit }, userSelection) =>
+    commit('setPostsDisplay', userSelection),
+}
+
+const getters = {
+  idLoggedIn: ({ user }) => user.isLoggedIn,
+  getUser: ({ user }) => user.userCredientials,
+  getListStyle: ({ listStyle }) => listStyle,
+}
+
 export const store = new Vuex.Store({
-  state: {
-    user: {
-      isLoggedIn: false,
-      userCredientials: {
-        userName: '',
-        userId: '',
-      },
-    },
-    listStyle: 'tile',
-  },
-  mutations: {
-    loginUser: ({ user }, { username, password }) => {
-      return { user, username, password }
-    },
-    logoutuser: ({ user }) => {
-      return user
-    },
-    setPostsDisplay(state, userSelection) {
-      state.listStyle = userSelection
-    },
-  },
-  actions: {
-    loginUser: ({ commit }) =>
-      commit('loginUser', {
-        username: 'testUser',
-        password: 'testPass',
-      }),
-    logoutUser: ({ commit }) => commit('logoutUser'),
-    setPostsDisplay: ({ commit }, userSelection) =>
-      commit('setPostsDisplay', userSelection),
-  },
-  getters: {
-    idLoggedIn: ({ user }) => user.isLoggedIn,
-    getUser: ({ user }) => user.userCredientials,
-    getListStyle: ({ listStyle }) => listStyle,
-  },
+  state,
+  mutations,
+  actions,
+  getters,
 })
